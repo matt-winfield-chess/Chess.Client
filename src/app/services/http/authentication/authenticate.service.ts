bd9890749bbf6e8a0ec41ee0a70e6a894de9f9cd
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-10-13 16:36:11 +0100

    Implement user log in functionality

---
+++ src/app/services/http/authentication/authenticate.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ConfigService } from '../../config/config.service';
+import { ApiResponse } from '../../../classes/models/api-response';
+import { LoginResponse } from '../../../classes/models/login-response';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthenticateService {
+
+	constructor(private http: HttpClient, private configService: ConfigService) { }
+
+	public async logIn(username: string, password: string): Promise<ApiResponse<LoginResponse>> {
+		let url = this.configService.getApiEndpoint('AUTHENTICATE');
+		return await this.http.post<ApiResponse<LoginResponse>>(url, {
+			username: username,
+			password: password
+		}).toPromise().catch(reason => {
+			return reason.error;
+		});
+	}
+}
