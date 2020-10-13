ee6b7b5089c21ce32d7e736155e967e00a851c4c
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-10-13 10:01:16 +0100

    Add navbar and some theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/gameplay/board/board.component';
 import { PieceComponent } from './components/gameplay/piece/piece.component';
+import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { CreateAccountPageComponent } from './pages/create-account-page/create-account-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GamePageComponent } from './pages/game-page/game-page.component';
+import { ThemeComponent } from './components/theming/theme/theme.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		BoardComponent,
-		PieceComponent
+		PieceComponent,
+		NavbarComponent,
+		CreateAccountPageComponent,
+		HomePageComponent,
+		GamePageComponent,
+		ThemeComponent
 	],
 	imports: [
 		BrowserModule,
