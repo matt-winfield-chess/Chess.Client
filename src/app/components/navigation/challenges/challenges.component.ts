82f18c61b6899d3463f2f80b3b4c8986e3177556
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-10-15 17:17:50 +0100

    Display challenges when sent in real time

--- src/app/components/navigation/challenges/challenges.component.ts
+++ src/app/components/navigation/challenges/challenges.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { ChallengeHubSignalRService } from '../../../services/signal-r/challenge-hub-signal-r.service';
 import { Challenge } from '../../../classes/models/challenge';
 import { SignalRMethod } from 'src/app/services/signal-r/signal-r-method';
+import { ChallengerColor } from 'src/app/enums/challenger-color.enum';
 
 @Component({
 	selector: 'app-challenges',
@@ -10,11 +11,13 @@ import { SignalRMethod } from 'src/app/services/signal-r/signal-r-method';
 })
 export class ChallengesComponent {
 
+	public activeChallenges: Challenge[] = [];
+
 	constructor(@Inject(ChallengeHubSignalRService) private challengeHubService: ChallengeHubSignalRService) {
 		this.challengeHubService.onMethod(SignalRMethod.NewChallenge, (challenge) => this.onChallengeRecieved(challenge));
 	}
 
 	private onChallengeRecieved(challenge: Challenge) {
-		console.log(challenge);
+		this.activeChallenges.push(challenge);
 	}
 }
