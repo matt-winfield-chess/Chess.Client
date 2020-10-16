a0cd058c34d46e4d9ddb4d9490dfca3c3e0d3d9e
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-10-16 09:12:51 +0100

    Update challenges when logging in/out. Allow dismissal of challenge notifications

--- src/app/components/navigation/challenges/challenges.component.ts
+++ src/app/components/navigation/challenges/challenges.component.ts
@@ -1,31 +1,57 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ChallengeHubSignalRService } from '../../../services/signal-r/challenge-hub-signal-r.service';
 import { Challenge } from '../../../classes/models/challenge';
 import { SignalRMethod } from 'src/app/services/signal-r/signal-r-method';
 import { ChallengerColor } from 'src/app/enums/challenger-color.enum';
+import { ChallengesService } from 'src/app/services/http/challenges/challenges.service'
+import { LoginStateService } from 'src/app/services/login-state.service';
 
 @Component({
 	selector: 'app-challenges',
 	templateUrl: './challenges.component.html',
 	styleUrls: ['./challenges.component.scss']
 })
-export class ChallengesComponent {
+export class ChallengesComponent implements OnInit {
 
-	public activeChallenges: Challenge[] = [
-		// {
-		// 	challenger: { id: 1, username: 'username1' },
-		// 	recipient: { id: 2, username: 'username2' },
-		// 	challengerColor: ChallengerColor.Random
-		// },
-		// {
-		// 	challenger: { id: 1, username: 'username1' },
-		// 	recipient: { id: 2, username: 'username3' },
-		// 	challengerColor: ChallengerColor.Random
-		// }
-	];
+	public activeChallenges: Challenge[] = [];
 
-	constructor(@Inject(ChallengeHubSignalRService) private challengeHubService: ChallengeHubSignalRService) {
+	constructor(@Inject(ChallengeHubSignalRService) private challengeHubService: ChallengeHubSignalRService,
+		@Inject(ChallengesService) private challengeService: ChallengesService,
+		@Inject(LoginStateService) private loginStateService: LoginStateService) {
 		this.challengeHubService.onMethod(SignalRMethod.NewChallenge, (challenge) => this.onChallengeRecieved(challenge));
+		this.loginStateService.subscribeToLogIn(() => this.syncChallenges());
+		this.loginStateService.subscribeToLogOut(() => this.onLogOut());
+	}
+
+	public async ngOnInit(): Promise<void> {
+		this.syncChallenges();
+	}
+
+	public accept(challenge: Challenge): void {
+
+	}
+
+	public async dismiss(challenge: Challenge): Promise<void> {
+		await this.challengeService.deleteChallenge(challenge.challenger.id, challenge.recipient.id);
+
+		let challengeIndex = this.activeChallenges.findIndex(c =>
+			c.challenger.id == challenge.challenger.id
+			&& c.recipient.id == challenge.recipient.id);
+
+		this.activeChallenges.splice(challengeIndex, 1);
+	}
+
+	private onLogOut(): void {
+		this.activeChallenges = [];
+	}
+
+	private async syncChallenges(): Promise<void> {
+		if (this.loginStateService.isLoggedIn()) {
+			let challengesResponse = await this.challengeService.getChallenges();
+			if (challengesResponse?.isSuccess) {
+				this.activeChallenges = challengesResponse.data;
+			}
+		}
 	}
 
 	private onChallengeRecieved(challenge: Challenge) {
---
+++ src/app/services/http/challenges/challenges.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApiResponse } from 'src/app/classes/models/api-response';
+import { Challenge } from 'src/app/classes/models/challenge';
+import { ConfigService } from '../../config/config.service';
+import { LoginStateService } from '../../login-state.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ChallengesService {
+
+	constructor(private http: HttpClient, private configService: ConfigService, private loginStateService: LoginStateService) { }
+
+	public async getChallenges(): Promise<ApiResponse<Challenge[]>> {
+		let url = this.configService.getApiEndpoint('RECEIVED_CHALLENGES');
+
+		let headers = new HttpHeaders({ 'Authorization': `Bearer ${this.loginStateService.getToken()}` });
+
+		return await this.http.get<ApiResponse<Challenge[]>>(url, { headers: headers }).toPromise().catch(reason => {
+			return reason.error;
+		});
+	}
+
+	public async deleteChallenge(challengerId: number, recipientId: number): Promise<ApiResponse<void>> {
+		let url = this.configService.getApiEndpoint('DELETE_CHALLENGE');
+		url += `/${challengerId}/${recipientId}`;
+
+		let headers = new HttpHeaders({ 'Authorization': `Bearer ${this.loginStateService.getToken()}` });
+
+		return await this.http.delete<ApiResponse<void>>(url, { headers: headers }).toPromise().catch(reason => {
+			return reason.error;
+		});
+	}
+}
