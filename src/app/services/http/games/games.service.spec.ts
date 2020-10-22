76e8960d94c297329dc35ef3b5539369758b16b4
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-10-22 20:00:11 +0100

    Fix unit tests

--- src/app/services/http/games/games.service.spec.ts
+++ src/app/services/http/games/games.service.spec.ts
@@ -1,16 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { GamesService } from './games.service';
 
 describe('GamesService', () => {
-  let service: GamesService;
+	let service: GamesService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GamesService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientModule]
+		});
+		service = TestBed.inject(GamesService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
