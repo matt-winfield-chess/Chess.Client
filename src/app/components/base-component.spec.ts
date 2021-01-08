2f2895a107a1e09e3491700ca5b9489359abed0a
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2021-01-08 16:53:19 +0000

    Fix unit tests

--- src/app/components/base-component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BaseComponent } from './base-component';
-
-describe('BasePageComponent', () => {
-	it('should create an instance', () => {
-		expect(new BaseComponent()).toBeTruthy();
-	});
-});
--- src/app/pages/user-settings/user-settings.component.spec.ts
+++ src/app/pages/user-settings/user-settings.component.spec.ts
@@ -1,25 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ToastrModule } from 'ngx-toastr';
 
 import { UserSettingsComponent } from './user-settings.component';
 
 describe('UserSettingsComponent', () => {
-  let component: UserSettingsComponent;
-  let fixture: ComponentFixture<UserSettingsComponent>;
+	let component: UserSettingsComponent;
+	let fixture: ComponentFixture<UserSettingsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserSettingsComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientModule, ToastrModule.forRoot()],
+			declarations: [UserSettingsComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserSettingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(UserSettingsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
