fb56c798ff3425d68cf0190fe6ad33da466bbee4
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2021-03-10 14:56:06 +0000

    Fix move history unit test

--- src/app/components/navigation/move-history/move-history.component.spec.ts
+++ src/app/components/navigation/move-history/move-history.component.spec.ts
@@ -1,25 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MoveHistoryComponent } from './move-history.component';
 
 describe('MoveHistoryComponent', () => {
-  let component: MoveHistoryComponent;
-  let fixture: ComponentFixture<MoveHistoryComponent>;
+	let component: MoveHistoryComponent;
+	let fixture: ComponentFixture<MoveHistoryComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MoveHistoryComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [MoveHistoryComponent],
+			imports: [RouterTestingModule]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MoveHistoryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MoveHistoryComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
